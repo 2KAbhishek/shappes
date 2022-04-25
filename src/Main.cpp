4cb52dde1663e08fc33717a5e7e679012f9a207e
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-25 08:41:19 +0530

    Update main method

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,22 +7,26 @@
 
 int main()
 {
-    Point p1(0, 0);
-    Point p2(0, 1);
-    Point p3(1, 0);
-    Point p4(1, 1);
-    Point p5(2, 2);
-    Point p6(3, 3);
+    Point p1(2, 6);
+    Point p2(1, 4);
+    Point p3(3, 3);
+    Point p4(4, 5);
+
+    Point p5(5, 3);
+    Point p6(5, 0);
+    Point p7(8, 0);
+
+    Point p8(5, 3);
+    Point p9(6, 1);
+    Point p10(8, 0);
 
     Rectangle r1(p1, p2, p3, p4);
-    Rectangle r2(p5, p6, p1, p2);
-    Triangle t1(p6, p2, p3);
-    Triangle t2(p4, p5, p6);
+    Triangle t1(p5, p6, p7);
+    Triangle t2(p8, p9, p10);
 
 
     std::vector<Shape *> shapeList;
     shapeList.push_back(&r1);
-    shapeList.push_back(&r2);
     shapeList.push_back(&t1);
     shapeList.push_back(&t2);
 
